75b87dbfa542ab6380abddcbea0f443ff1f8b3ab
Kirill Bazhenov <bazhenovc@bazhenovc-laptop>
2013-05-10 01:31:53 +0300

    first commit

---
+++ LibGraphics/Renderers/OpenGLRenderer/Wm5GlPlugin.cpp
@@ -0,0 +1,41 @@
+// Geometric Tools, LLC
+// Copyright (c) 1998-2012
+// Distributed under the Boost Software License, Version 1.0.
+// http://www.boost.org/LICENSE_1_0.txt
+// http://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
+//
+// File Version: 5.0.0 (2010/01/01)
+
+#include "Wm5GlExtensions.h"
+#include <cassert>
+#include <cstdio>
+
+// Disable the MSVC8 warnings about deprecated functions.
+#ifdef _MSC_VER
+#pragma warning( push )
+#pragma warning( disable : 4996 )
+#endif
+
+//----------------------------------------------------------------------------
+void ReportNullFunction (const char*)
+{
+    assert(false);
+}
+//----------------------------------------------------------------------------
+void ReportGLError (const char* acFunction)
+{
+    GLenum code = glGetError();
+    while (code != GL_NO_ERROR)
+    {
+        const char* errorString = GTGetErrorString(code);
+        (void)errorString;
+        assert(false);
+        code = glGetError();
+    }
+    (void)acFunction;
+}
+//----------------------------------------------------------------------------
+
+#ifdef _MSC_VER
+#pragma warning( pop )
+#endif
